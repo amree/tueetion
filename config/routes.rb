b876c16cd6827f9c8593cc54d6f82189f52c44e3
amree <mohd.amree@gmail.com>
2013-09-03 02:17:39 +0800

    Generated files using rails generate devise:install and edit some based on the recommendation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Tueetion::Application.routes.draw do
   resources :centers
 
+  root "centers#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
