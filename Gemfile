b8da2b51dbdf5e66bd72d3ab8b8941c78d044c8e
amree <mohd.amree@gmail.com>
2013-09-19 23:46:04 +0800

    Removed Foundation related codes and files

--- Gemfile
+++ Gemfile
@@ -27,7 +27,6 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
-gem 'zurb-foundation', '~> 4.2.3'
 gem 'devise', '~> 3.0.3'
 
 group :doc do
--- config/initializers/form_errors.rb
+++
@@ -1,8 +0,0 @@
-ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
-  unless html_tag =~ /^<label/
-    errors = Array(instance.error_message).join(',')
-    %(<div class="error">#{html_tag}<small class="error">&nbsp;#{errors}</small></div>).html_safe
-  else
-    %(<div class="error">#{html_tag}</div>).html_safe
-  end
-end
