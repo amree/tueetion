4ee7a6a634439c228a711fcde0034df37a71c2f7
amree <mohd.amree@gmail.com>
2013-10-19 12:46:41 +0800

    Updated Student's view. Closes #8

--- app/models/enroll.rb
+++ app/models/enroll.rb
@@ -1,8 +1,8 @@
 class Enroll < ActiveRecord::Base
   belongs_to :student
   belongs_to :enrollable, polymorphic: true
-  has_many :student_subjects
   has_many :enroll_subjects
+  has_many :subjects, through: :enroll_subjects
 
   accepts_nested_attributes_for :enroll_subjects
 end
