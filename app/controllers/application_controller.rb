e87547ce697999eb631e551f5d4b197c2ec03ad2
amree <mohd.amree@gmail.com>
2013-10-26 22:02:16 +0800

    Added single layout support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  layout :layout_by_resource
+
   before_filter :authenticate_user!
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "single"
+    else
+      "application"
+    end
+  end
 
   private
 
