a28dc4e24c1250c8d68740e177f2109dc2fb4e80
amree <mohd.amree@gmail.com>
2013-10-31 23:02:25 +0800

    Fixed failing test due to outdated request codes. Refs #18

--- test/controllers/settings/accounts_controller_test.rb
+++ test/controllers/settings/accounts_controller_test.rb
@@ -2,16 +2,20 @@ require 'test_helper'
 
 class Settings::AccountsControllerTest < ActionController::TestCase
   setup do
+    @user = users(:user)
+
     sign_in users(:user)
   end
 
   test "should get edit profile" do
     get :edit
+
     assert_response :success
   end
 
   test "should get update profile" do
-    patch :update
-    assert_response :success
+    patch :update, user: { first_name: @user.first_name, last_name: @user.last_name, email: @user.email }
+
+    assert_redirected_to edit_settings_accounts_path
   end
 end
