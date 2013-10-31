e2483c01a1b599a88b14d18b43b193affe441e31
amree <mohd.amree@gmail.com>
2013-10-31 23:15:06 +0800

    Fixed duplicated test name

--- test/controllers/settings/accounts_controller_test.rb
+++ test/controllers/settings/accounts_controller_test.rb
@@ -13,7 +13,7 @@ class Settings::AccountsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get update profile" do
+  test "should update profile" do
     patch :update, user: { first_name: @user.first_name, last_name: @user.last_name, email: @user.email }
 
     assert_redirected_to edit_settings_accounts_path
