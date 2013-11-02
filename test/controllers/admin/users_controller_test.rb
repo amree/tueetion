dc0ec0e85d427698b8ff18279806909054c4cbf0
amree <mohd.amree@gmail.com>
2013-11-02 12:37:34 +0800

    Removed temporary tests

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -1,19 +1,4 @@
 require 'test_helper'
 
 class Admin::UsersControllerTest < ActionController::TestCase
-  setup do
-    sign_in users(:admin)
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
-
-  test "should get show" do
-    @user = users(:user)
-
-    get :show, id: @user
-    assert_response :success
-  end
 end
