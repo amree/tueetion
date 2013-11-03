d3d5bc7f0cbc40850efd4f7109a9b684f8b32b08
amree <mohd.amree@gmail.com>
2013-11-03 11:10:12 +0800

    Adde validation and key generator to Invitation

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,3 +1,21 @@
 class Invitation < ActiveRecord::Base
   belongs_to :center
+
+  before_validation :set_key
+
+  validates :email, uniqueness: true
+
+  protected
+
+  def set_key
+
+    self.key = generate_key
+    while Invitation.find_by_key(key).present?
+      self.key = generate_key
+    end
+  end
+
+  def generate_key
+    rand(36**10).to_s(36)
+  end
 end
--- test/models/invitation_test.rb
+++ test/models/invitation_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class InvitationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @center = centers(:pakar)
+    @unused = invitations(:unused)
+  end
+
+  test "should have created a key for an invitation" do
+    invitation = Invitation.new
+    invitation.center_id = @center.id
+    invitation.email = "random@mail.com"
+    invitation.save
+
+    assert invitation.key.present?
+  end
+
+  test "email should be unique" do
+    dup = @unused.dup
+
+    assert dup.invalid?
+    assert dup.errors[:email].present?
+  end
 end
