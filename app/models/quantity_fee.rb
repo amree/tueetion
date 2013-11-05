fdd5a220fa6009c7818ae299a5ca49958ba0ce14
amree <mohd.amree@gmail.com>
2013-11-05 13:49:04 +0800

    Added validations for QuantityFee

--- app/models/quantity_fee.rb
+++ app/models/quantity_fee.rb
@@ -2,4 +2,10 @@ class QuantityFee < ActiveRecord::Base
   belongs_to :subject
   belongs_to :center
   has_many :enrolls, as: :enrollable
+
+  validates :name, presence: true
+  validates :name, uniqueness: { scope: :center_id }
+  validates :subject_id, presence: true
+  validates :subject, presence: true
+  validates :price, numericality: { greater_than: 0 }
 end
--- test/models/quantity_fee_test.rb
+++ test/models/quantity_fee_test.rb
@@ -1,7 +1,59 @@
 require 'test_helper'
 
 class QuantityFeeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @bm = quantity_fees(:bm_pakar)
+  end
+
+  test "name should not be blank" do
+    dup = @bm.dup
+    dup.name = nil
+
+    dup.valid?
+
+    assert dup.errors[:name].present?
+  end
+
+  test "name should be unique" do
+    dup = @bm.dup
+
+    dup.valid?
+
+    assert dup.errors[:name].present?
+  end
+
+  test "subject should not be blank" do
+    @bm.subject_id = nil
+    @bm.valid?
+
+    assert @bm.errors[:subject_id].present?
+  end
+
+  test "subject should be valid for current center" do
+    @bm.subject_id = 999
+    @bm.valid?
+
+    assert @bm.errors[:subject].present?
+  end
+
+  test "price should not be blank" do
+    @bm.price = nil
+    @bm.valid?
+
+    assert @bm.errors[:price].present?
+  end
+
+  test "price should not be negative" do
+    @bm.price = -1
+    @bm.valid?
+
+    assert @bm.errors[:price].present?
+  end
+
+  test "price should be able to accept precision number" do
+    @bm.price = 10.10
+    @bm.valid?
+
+    assert @bm.errors[:price].blank?
+  end
 end
