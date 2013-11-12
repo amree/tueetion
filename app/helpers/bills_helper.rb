38bfab2e9f6d00e380d6be4b699b847193c4256b
amree <mohd.amree@gmail.com>
2013-11-12 22:32:29 +0800

    Updated Student's scaffold UI, closes #44

--- app/helpers/bills_helper.rb
+++ app/helpers/bills_helper.rb
@@ -1,2 +1,17 @@
 module BillsHelper
+  def bill_status_label(status, true_val, false_val)
+    attributes = {}
+    content = nil
+
+    if status
+      attributes["class"] = "label label-success"
+      content = true_val
+    else
+      attributes["class"] = "label label-danger"
+      content = false_val
+    end
+
+    # content_tag "span", attributes, content
+    content_tag :span, content, attributes
+  end
 end
