95ff3e620d57a0fa8c0d6b580d563650e77bac77
amree <mohd.amree@gmail.com>
2013-11-22 18:54:24 +0800

    Added association for CombinationFee to Subject

--- app/models/combination_fee.rb
+++ app/models/combination_fee.rb
@@ -2,6 +2,7 @@ class CombinationFee < ActiveRecord::Base
   belongs_to :center
   has_many :combination_item_fees
   has_many :enrolls, as: :enrollable
+  has_many :subjects, through: :combination_item_fees
 
   validates :center_id, presence: true
   validates :center, presence: true
