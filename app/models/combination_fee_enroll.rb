05c5fdcf926fd604dba9e56a2eb4d45603b295b5
amree <mohd.amree@gmail.com>
2013-11-30 02:56:05 +0800

    Regenerate subject whenever CombinationFeeEnroll changed

--- app/models/combination_fee_enroll.rb
+++ app/models/combination_fee_enroll.rb
@@ -3,6 +3,7 @@ class CombinationFeeEnroll < Enroll
 
   before_validation :set_enrollable_type
   after_create :generate_enrolled_subjects
+  after_update :regenerate_enrolled_subjects, if: "self.enrollable_id_changed?"
 
   def name
     enrollable.name
@@ -19,6 +20,25 @@ class CombinationFeeEnroll < Enroll
     self.enrollable.subjects.each do |subject|
       enroll_subject = self.enroll_subjects.find_or_initialize_by(subject: subject)
       enroll_subject.save!
+    end
+  end
+
+  # TODO: Service Object?
+  def regenerate_enrolled_subjects
+    subject_ids = self.enrollable.subject_ids
+
+    # Create new Enroll Subject if not exits
+    subject_ids.each do |subject|
+      enroll_subject = self.enroll_subjects.find_or_initialize_by(subject_id: subject)
+      enroll_subject.save!
+    end
+
+    # Delete old Enroll Subject if it's not from the Combination Fee
+    difference = self.subject_ids - subject_ids
+
+    difference.each do |subject|
+      enroll_subject = self.enroll_subjects.find_by(subject_id: subject)
+      enroll_subject.destroy
     end
   end
 end
