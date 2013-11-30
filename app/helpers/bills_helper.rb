211d403c9a46bc33950b6cf991b58af3b162dae3
amree <mohd.amree@gmail.com>
2013-11-30 11:17:35 +0800

    Centralised bill status helper

--- app/helpers/bills_helper.rb
+++ app/helpers/bills_helper.rb
@@ -13,4 +13,28 @@ module BillsHelper
 
     content_tag :span, content, attributes
   end
+
+  def all_bill_status(bill)
+
+    status = ''
+
+    if !bill.is_active
+      status = status + bill_status_label(bill.is_active, "", "INACTIVE")
+      status = add_spacer(status)
+    end
+
+    status = status + bill_status_label(bill.is_paid, "PAID", "UNPAID")
+    status = add_spacer(status)
+
+    if bill.is_overdue
+      status = status + bill_status_label(bill.is_overdue, "", "OVERDUE")
+      status = add_spacer(status)
+    end
+
+    raw(status)
+  end
+
+  def add_spacer(status)
+    status = status + "&nbsp;"
+  end
 end
