048776150f0a35719895087a791d0c36f6c7d3b3
amree <mohd.amree@gmail.com>
2013-11-30 12:08:35 +0800

    Only ItemBill in unpaid Bill can be edited

--- app/models/bill_item.rb
+++ app/models/bill_item.rb
@@ -1,3 +1,13 @@
 class BillItem < ActiveRecord::Base
   belongs_to :bill
+
+  validate :must_not_be_paid, unless: "self.new_record?"
+
+  protected
+
+  def must_not_be_paid
+    if self.bill.is_paid
+      errors.add(:base, "Only unpaid bill can be modified.")
+    end
+  end
 end
--- test/models/bill_item_test.rb
+++ test/models/bill_item_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class BillItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @bill_item = bill_items(:bill_item)
+  end
+
+  test "should not be able to edit if it's paid" do
+    # Mock so that bill is marked as paid
+    Bill.any_instance.stubs(:is_paid).returns(true)
+
+    bill = @bill_item.bill
+    @bill_item.amount = 100
+
+    assert @bill_item.invalid?
+    assert @bill_item.errors[:base].present?
+  end
 end
