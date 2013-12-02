09e48937dbed4888b157c139859f221f2e13b4a8
amree <mohd.amree@gmail.com>
2013-12-03 01:06:11 +0800

    Make sure update is_paid status after new payment has been made

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,11 +3,24 @@ class Payment < ActiveRecord::Base
 
   validate :less_than_bill_balance
 
+  after_create  :update_bill_paid_status
+  after_destroy :update_bill_paid_status
+
   protected
 
   def less_than_bill_balance
-    if self.amount > self.bill.payments.sum(:amount)
+    if self.amount > (self.bill.total_amount - self.bill.total_amount_paid)
       errors.add(:amount, "The amount should not be more than the bill's balance")
+    end
+  end
+
+  def update_bill_paid_status
+    if self.bill.total_amount_paid == self.bill.total_amount
+      self.bill.is_paid = true
+      self.bill.save
+    else
+      self.bill.is_paid = false
+      self.bill.save
     end
   end
 end
--- test/models/payments_test.rb
+++ test/models/payments_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class PaymentsTest < ActiveSupport::TestCase
   setup do
+    @bill = bills(:bill)
     @payment = payments(:payment)
   end
 
@@ -12,5 +13,20 @@ class PaymentsTest < ActiveSupport::TestCase
     payment.paid_at = DateTime.now
 
     assert payment.invalid?
+  end
+
+  test "should mark bill as paid when all payment has been made" do
+    payment = Payment.new
+    payment.bill = @payment.bill
+    payment.amount = 70
+    payment.paid_at = DateTime.now
+
+    assert payment.save
+    assert payment.bill.is_paid
+
+    payment.destroy
+    @bill.reload
+
+    assert !@bill.is_paid
   end
 end
