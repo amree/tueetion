818966ff59c4636f2455c7f8d69d6dfa11facfb8
amree <mohd.amree@gmail.com>
2013-12-02 23:22:37 +0800

    Updated payment's UI

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,17 +1,18 @@
 class PaymentsController < ApplicationController
   before_action :set_bill
+  before_action :set_student
   before_action :set_payment, only: [:destroy]
   before_action :payment_params, only: [:create]
 
   def new
+    @payment = Payment.new
   end
 
   def create
     payment = @bill.payments.new(payment_params)
 
     if payment.save
-      # TODO: Foward to [@bill.student, @bill]
-      redirect_to @bill.student, notice: 'Payment was successfully made.'
+      redirect_to @bill, notice: 'Payment was successfully made.'
     else
       render action: 'new'
     end
@@ -21,24 +22,28 @@ class PaymentsController < ApplicationController
   # DELETE /payments/1.json
   def destroy
     @payment.destroy
+
     respond_to do |format|
-      # TODO: Foward to [@bill.student, @bill]
-      format.html { redirect_to @bill.student }
+      format.html { redirect_to @bill, notice: 'Payment was successfully delted' }
       format.json { head :no_content }
     end
   end
 
   private
 
-    def set_bill
-      @bill = current_center.bills.find(params[:bill_id])
-    end
+  def set_bill
+    @bill = current_center.bills.find(params[:bill_id])
+  end
 
-    def set_payment
-      @payment = @bill.payments.find(params[:id])
-    end
+  def set_student
+    @student = @bill.student
+  end
 
-    def payment_params
-      params.require(:payment).permit(:amount, :paid_at)
-    end
+  def set_payment
+    @payment = @bill.payments.find(params[:id])
+  end
+
+  def payment_params
+    params.require(:payment).permit(:amount, :paid_at)
+  end
 end
