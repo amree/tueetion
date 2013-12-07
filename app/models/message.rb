d735525653ac7d59f11807aea3d9d9b3f00282e4
amree <mohd.amree@gmail.com>
2013-12-07 23:18:57 +0800

    Added callbacks and tests to Message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,60 @@
 class Message < ActiveRecord::Base
   belongs_to :center
   belongs_to :student
+
+  before_validation :set_phone_number, if: "self.student.present?"
+  before_validation :set_status_to_new, if: "self.new_record?"
+
+  before_validation :validate_center
+  before_validation :validate_student
+  before_validation :validate_phone_number
+  before_validation :validate_content
+
+  protected
+
+  def set_phone_number
+    if self.student.phone.present?
+      self.phone_number = self.student.phone
+    end
+  end
+
+  def set_status_to_new
+    self.status = "new"
+  end
+
+  def validate_center
+    if self.center.nil?
+      set_failed_messages("Center is invalid")
+    end
+  end
+
+  def validate_student
+    if self.student.nil?
+      set_failed_messages("Student is invalid")
+    end
+  end
+
+  def validate_phone_number
+    if self.phone_number.nil?
+      set_failed_messages("Phone number is invalid")
+    end
+  end
+
+  def validate_content
+    if self.content.nil?
+      set_failed_messages("Content is invalid")
+    end
+  end
+
+  private
+
+  def set_failed_messages(msg)
+    if self.failed_messages.blank?
+      self.failed_messages = msg
+    else
+      self.failed_messages = self.failed_messages + "," + msg
+    end
+
+    self.status = "invalid"
+  end
 end
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,69 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @ali = students(:ali)
+    @new_message = messages(:new_message)
+  end
+
+  test "phone number should not be empty" do
+    message = Message.new
+    message.student = @ali
+    message.content = "foo bar"
+
+    message.save
+
+    assert_not_nil message.phone_number
+  end
+
+  test "status should be new for newly created message" do
+    message = @new_message.dup
+    message.status = nil
+
+    assert message.save
+    assert_equal "new", message.status
+  end
+
+  test "status should not be changed to new when edited" do
+    @new_message.status = "queued"
+
+    assert @new_message.save
+    assert_equal @new_message.status, "queued"
+  end
+
+  test "message should be saved with error if center is invalid" do
+    message = Message.new
+    message.center_id = nil
+
+    assert message.save
+    assert message.failed_messages.match 'Center is invalid'
+    assert "invalid", message.status
+  end
+
+  test "message should be saved with error if student is invalid" do
+    message = Message.new
+    message.student_id = nil
+
+    assert message.save
+    assert message.failed_messages.match 'Student is invalid'
+    assert "invalid", message.status
+  end
+
+  test "message should be saved with error if phone number is invalid" do
+    message = Message.new
+    message.phone_number = nil
+
+    assert message.save
+    assert message.failed_messages.match 'Phone number is invalid'
+    assert "invalid", message.status
+  end
+
+  test "message should be saved with error if content is invalid" do
+    message = Message.new
+    message.content = nil
+
+    assert message.save
+    assert message.failed_messages.match 'Content is invalid'
+    assert "invalid", message.status
+  end
 end
