b063b027e8a8517ef142a9a56fa23de19eac4022
amree <mohd.amree@gmail.com>
2013-12-09 21:50:55 +0800

    Should only work if the message still exists

--- app/workers/sms_worker.rb
+++ app/workers/sms_worker.rb
@@ -4,13 +4,15 @@ class SmsWorker
   def self.perform(id)
     message = Message.find(id)
 
-    to = message.phone_number
-    body = message.content
+    if message.present?
+      to = message.phone_number
+      body = message.content
 
-    client = Twilio::REST::Client.new TWILIO_CONFIG['sid'], TWILIO_CONFIG['token']
-    rs =  client.account.messages.create from: TWILIO_CONFIG['from'], to: to, body: body, status_callback: "http://#{TWILIO_CONFIG['callback_host']}/callbacks/twilio"
+      client = Twilio::REST::Client.new TWILIO_CONFIG['sid'], TWILIO_CONFIG['token']
+      rs =  client.account.messages.create from: TWILIO_CONFIG['from'], to: to, body: body, status_callback: "http://#{TWILIO_CONFIG['callback_host']}/callbacks/twilio"
 
-    message.sid = rs.sid
-    message.save
+      message.sid = rs.sid
+      message.save
+    end
   end
 end
