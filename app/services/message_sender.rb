03c08f28874a1176fb1b31a00cd5b52a4a06cb7d
amree <mohd.amree@gmail.com>
2013-12-23 10:05:54 +0800

    Moved sending SMS to work as a service

---
+++ app/services/message_sender.rb
@@ -0,0 +1,48 @@
+require 'message_processor'
+
+class MessageSender
+  def initialize(message)
+    @message = message
+  end
+
+  def send
+    # Parse @message first
+    processed_message = MessageProcessor.new(@message)
+    @message.processed_content = processed_message.content
+
+    # Search for free credit
+    credit = Credit.available(@message.center_id).first
+    credit_balance = credit.amount - credit.used
+
+    # Only send if there are enough credits available
+    if credit && credit_balance >= processed_message.sms_count
+
+      # Build message usages
+      Array.new(processed_message.sms_count).each do
+        @message.credit_usages.build(credit_id: credit.id)
+      end
+
+      # Prepare for @message sending
+      to = @message.phone_number
+      body = @message.processed_content
+
+      client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
+
+      rs =  client.account.messages.create(
+              from: TWILIO_CONFIG['from'],
+              to: to,
+              body: body,
+              status_callback: "http://#{TWILIO_CONFIG['callback_host']}/callbacks/twilio")
+
+      @message.sid = rs.sid
+
+      ActiveRecord::Base.transaction do
+        credit.save
+        @message.save
+      end
+    else
+      @message.status = "no credit"
+      @message.save
+    end
+  end
+end
--- app/workers/sms_worker.rb
+++ app/workers/sms_worker.rb
@@ -7,23 +7,8 @@ class SmsWorker
     message = Message.find(id)
 
     if message.present?
-      # Parse message first
-      mp = MessageProcessor.new(message)
-      message.processed_content = mp.content
-
-      # Prepare for sending message
-      to = message.phone_number
-      body = message.processed_content
-
-      client = Twilio::REST::Client.new TWILIO_CONFIG['sid'], TWILIO_CONFIG['token']
-      rs =  client.account.messages.create(
-              from: TWILIO_CONFIG['from'],
-              to: to,
-              body: body,
-              status_callback: "http://#{TWILIO_CONFIG['callback_host']}/callbacks/twilio")
-
-      message.sid = rs.sid
-      message.save
+      sender = MessageSender.new(message)
+      sender.send
     end
   end
 end
