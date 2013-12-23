a4255b199766da7c3d6686f3eae45706b608ce01
amree <mohd.amree@gmail.com>
2013-12-23 10:09:02 +0800

    Make sure no double message send

--- app/services/message_sender.rb
+++ app/services/message_sender.rb
@@ -6,43 +6,45 @@ class MessageSender
   end
 
   def send
-    # Parse @message first
-    processed_message = MessageProcessor.new(@message)
-    @message.processed_content = processed_message.content
+    if @message.status != 'sent'
+      # Parse @message first
+      processed_message = MessageProcessor.new(@message)
+      @message.processed_content = processed_message.content
 
-    # Search for free credit
-    credit = Credit.available(@message.center_id).first
-    credit_balance = credit.amount - credit.used
+      # Search for free credit
+      credit = Credit.available(@message.center_id).first
+      credit_balance = credit.amount - credit.used
 
-    # Only send if there are enough credits available
-    if credit && credit_balance >= processed_message.sms_count
+      # Only send if there are enough credits available
+      if credit && credit_balance >= processed_message.sms_count
 
-      # Build message usages
-      Array.new(processed_message.sms_count).each do
-        @message.credit_usages.build(credit_id: credit.id)
-      end
+        # Build message usages
+        Array.new(processed_message.sms_count).each do
+          @message.credit_usages.build(credit_id: credit.id)
+        end
 
-      # Prepare for @message sending
-      to = @message.phone_number
-      body = @message.processed_content
+        # Prepare for @message sending
+        to = @message.phone_number
+        body = @message.processed_content
 
-      client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
+        client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
 
-      rs =  client.account.messages.create(
-              from: TWILIO_CONFIG['from'],
-              to: to,
-              body: body,
-              status_callback: "http://#{TWILIO_CONFIG['callback_host']}/callbacks/twilio")
+        rs =  client.account.messages.create(
+                from: TWILIO_CONFIG['from'],
+                to: to,
+                body: body,
+                status_callback: "http://#{TWILIO_CONFIG['callback_host']}/callbacks/twilio")
 
-      @message.sid = rs.sid
+        @message.sid = rs.sid
 
-      ActiveRecord::Base.transaction do
-        credit.save
+        ActiveRecord::Base.transaction do
+          credit.save
+          @message.save
+        end
+      else
+        @message.status = "no credit"
         @message.save
       end
-    else
-      @message.status = "no credit"
-      @message.save
     end
   end
 end
