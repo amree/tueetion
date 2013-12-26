5253b43c7baa1b7ca86c31d797871ad4f2f0c85b
amree <mohd.amree@gmail.com>
2013-12-26 08:02:58 +0800

    Make sure Enroll got deleted if EnrollSubject destroyed

--- app/models/enroll_subject.rb
+++ app/models/enroll_subject.rb
@@ -1,4 +1,16 @@
 class EnrollSubject < ActiveRecord::Base
   belongs_to :enroll
   belongs_to :subject
+
+  after_destroy :destroy_enroll
+
+  protected
+
+  def destroy_enroll
+    # Delete enroll if it's the only record
+    # Do not delete enroll if it has siblings, especially in CombinationFee
+    if EnrollSubject.where(enroll_id: enroll_id).count == 0
+      Enroll.where(id: enroll_id).first.destroy
+    end
+  end
 end
--- test/models/enroll_subject_test.rb
+++ test/models/enroll_subject_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class EnrollSubjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @quantity_fee = enroll_subjects(:ali_quantity_fee_subject)
+    @combination_fee1 = enroll_subjects(:ali_combination_fee_subject_bm)
+    @combination_fee2 = enroll_subjects(:ali_combination_fee_subject_bi)
+  end
+
+  test "should destroy enroll (for QuantityFee)" do
+    @quantity_fee.destroy
+
+    assert Enroll.where(id: @quantity_fee.enroll_id).blank?
+  end
+
+  test "should destroy enroll (for CombinationFee)" do
+    @combination_fee1.destroy
+    @combination_fee2.destroy
+
+    assert Enroll.where(id: @combination_fee2.enroll_id).blank?
+  end
 end
