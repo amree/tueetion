26f581692dd0ac80d72ccdc5a1fb912027de61d0
amree <mohd.amree@gmail.com>
2014-02-01 11:35:47 +0800

    Fixed, should've limit the result only to the current user

--- app/helpers/reports/subjects_helper.rb
+++ app/helpers/reports/subjects_helper.rb
@@ -1,6 +1,6 @@
 module Reports::SubjectsHelper
   def subject_enrollment_chart_data
-    Subject.order(:name).map do |subject|
+    current_user.center.subjects.order(:name).map do |subject|
       [subject.name, EnrollSubject.where(subject_id: subject.id).count]
     end
   end
