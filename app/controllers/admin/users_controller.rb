d533c6f75bada65d25f484a1b156f9a695e4d3ae
amree <mohd.amree@gmail.com>
2014-03-02 22:41:11 +0800

    Added the first admin UI, yeay

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,6 +2,7 @@ class Admin::UsersController < ApplicationController
   before_action :require_admin
 
   def index
+    @users = User.joins(:center).order("users.created_at DESC").to_a
   end
 
   def show
