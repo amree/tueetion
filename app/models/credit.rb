e3a899c763f9eeab5f8eb44e06bd3fb309912100
amree <mohd.amree@gmail.com>
2014-03-03 08:26:17 +0800

    Fixed (partially) failing tests due to changes in database schema

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -2,5 +2,5 @@ class Credit < ActiveRecord::Base
   belongs_to :center
   has_many :credit_usages
 
-  scope :available, -> { where("amount > used").order(:paid_at) }
+  scope :available, -> { where("amount > used") }
 end
