e80b9b74cf3decefc2368390b258f1fe99bf48af
amree <mohd.amree@gmail.com>
2014-03-04 16:02:20 +0800

    Added relationship between Credit and Transaction. Refs #81

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,6 +1,9 @@
 class Credit < ActiveRecord::Base
   belongs_to :center
   has_many :credit_usages
+  has_many :transactions, as: :transactable, dependent: :destroy
+
+  accepts_nested_attributes_for :transactions
 
   scope :available, -> { where("amount > used") }
   scope :by_latest, -> { order("credits.created_at DESC") }
