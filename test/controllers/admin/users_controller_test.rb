b9c1861eb4ba220efa045314cd75044bdcc87ffd
amree <mohd.amree@gmail.com>
2014-03-04 16:03:38 +0800

    Added test for Admin's User controller test

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -1,4 +1,22 @@
 require 'test_helper'
 
 class Admin::UsersControllerTest < ActionController::TestCase
+  setup do
+    @admin = users(:admin)
+
+    sign_in @admin
+  end
+
+  test "should get index" do
+    get :index
+
+    assert_response :success
+    assert_not_nil assigns(:users)
+  end
+
+  test "should show user" do
+    get :show, id: @admin
+
+    assert_response :success
+  end
 end
