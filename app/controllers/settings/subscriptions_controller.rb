061c2ad3f5e76cf93da84bcdf0adc5f76f6ce7dd
amree <mohd.amree@gmail.com>
2014-03-18 04:55:18 +0800

    Display Subscription information

--- app/controllers/settings/subscriptions_controller.rb
+++ app/controllers/settings/subscriptions_controller.rb
@@ -2,5 +2,10 @@ class Settings::SubscriptionsController < ApplicationController
   before_action :check_center
 
   def index
+    @subscriptions = current_center.subscriptions
+  end
+
+  def show
+    @subscription = current_center.subscriptions.find(params[:id])
   end
 end
