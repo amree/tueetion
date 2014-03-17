454e316d56f8bf64391f891fd841281b59f26513
amree <mohd.amree@gmail.com>
2014-03-18 04:54:38 +0800

    Added route to Setting/Subscription#show

--- test/controllers/settings/subscriptions_controller_test.rb
+++ test/controllers/settings/subscriptions_controller_test.rb
@@ -2,11 +2,19 @@ require 'test_helper'
 
 class Settings::SubscriptionsControllerTest < ActionController::TestCase
   setup do
+    @subscription = subscriptions(:pakar_subscription)
+
     sign_in users(:user)
   end
 
   test "should get index" do
     get :index
+
+    assert_response :success
+  end
+
+  test "should get subscription" do
+    get :show, id: @subscription
 
     assert_response :success
   end
