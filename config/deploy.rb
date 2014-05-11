c5f8b21b46e49197d19112bfba817a8c9b08bc2a
amree <mohd.amree@gmail.com>
2014-05-11 17:40:07 +0800

    First working version of Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,44 +2,35 @@
 lock '3.2.1'
 
 set :application, 'tueetion'
-set :repo_url, 'git@github.com:amree/tueetion.git'
 set :deploy_user, 'amree'
 
-# Setup rbenv
+# setup repo details
+set :scm, :git
+set :repo_url, 'git@github.com:amree/tueetion.git'
+
+# setup rvm.
 set :rbenv_type, :user
 set :rbenv_ruby, '2.1.0'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+# how many old releases do we want to keep, not much
+set :keep_releases, 5
 
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
+# files we want symlinking to specific entries in shared
 set :linked_files, %w{config/database.yml config/twilio.yml}
 
-# Default value for linked_dirs is []
+# dirs we want symlinking to shared
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# Default value for linked_dirs is []
+linked_dirs = Set.new(fetch(:linked_dirs, [])) # https://github.com/capistrano/rails/issues/52
+linked_dirs.merge(%w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system})
+set :linked_dirs, linked_dirs.to_a
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+# what specs should be run before deployment is allowed to
+# continue, see lib/capistrano/tasks/run_tests.cap
+set :tests, []
 
 # which config files should be copied by deploy:setup_config
 # see documentation in lib/capistrano/tasks/setup_config.cap
@@ -58,17 +49,20 @@ set(:executable_config_files, %w(
   unicorn_init.sh
 ))
 
+
 # files which need to be symlinked to other parts of the
 # filesystem. For example nginx virtualhosts, log rotation
-# init scripts etc.
+# init scripts etc. The full_app_name variable isn't
+# available at this point so we use a custom template {{}}
+# tag and then add it at run time.
 set(:symlinks, [
   {
     source: "nginx.conf",
-    link: "/etc/nginx/sites-enabled/#{fetch(:full_app_name)}"
+    link: "/etc/nginx/sites-enabled/{{full_app_name}}"
   },
   {
     source: "unicorn_init.sh",
-    link: "/etc/init.d/unicorn_#{fetch(:full_app_name)}"
+    link: "/etc/init.d/unicorn_{{full_app_name}}"
   }
 ])
 
@@ -82,11 +76,10 @@ namespace :deploy do
   before :deploy, "deploy:check_revision"
 
   # only allow a deploy with passing tests to deployed
-  before :deploy, "deploy:run_tests"
+  # before :deploy, "deploy:run_tests"
 
   # compile assets locally then rsync
   after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
-
   after :finishing, 'deploy:cleanup'
 
   # remove the default nginx configuration as it will tend
