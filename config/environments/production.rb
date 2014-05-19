fc5ab3d47317e0ed676ed945f73c6423a5bc6db4
amree <mohd.amree@gmail.com>
2014-05-19 23:03:53 +0800

    Should've already used value from secrets.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,8 @@ Tueetion::Application.configure do
     :address   => "smtp.mandrillapp.com",
     :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
     :enable_starttls_auto => true, # detects and uses STARTTLS
-    :user_name => ENV["MANDRILL_USERNAME"],
-    :password  => ENV["MANDRILL_PASSWORD"], # SMTP password is any valid API key
+    :user_name => Rails.application.secrets.mandrill_username,
+    :password  => Rails.application.secrets.mandrill_password,
     :authentication => 'login', # Mandrill supports 'plain' or 'login'
     :domain => 'tueetion.com', # your domain to identify your server when conn
   }
