06c0d0f123a297ec36432f70e71df77b7483baf5
amree <mohd.amree@gmail.com>
2014-05-23 16:29:18 +0800

    Added listing for Payment. Closes #109

--- app/controllers/reports/payments_controller.rb
+++ app/controllers/reports/payments_controller.rb
@@ -3,5 +3,6 @@ class Reports::PaymentsController < ApplicationController
 
   def index
     @current_year = Time.zone.now.strftime '%Y'
+    @payments = Payment.includes(:bill).includes(:user).where(bills: { center_id: current_center.id }).order("payments.created_at DESC")
   end
 end
