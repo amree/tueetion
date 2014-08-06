86f8304adaf90742c4859709ad7c2ea037d87948
amree <mohd.amree@gmail.com>
2014-08-07 01:16:14 +0800

    Added initial Country data (for Malaysia)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,14 @@
 # Default Admin user
-User.create(first_name: "Mohd Amree", last_name: "Che Mohd Zaid", email: "mohd.amree@gmail.com", password: "password", password_confirmation: "password", access_level: 100)
+User.create(
+      first_name: "Mohd Amree",
+      last_name: "Che Mohd Zaid",
+      email: "mohd.amree@gmail.com",
+      password: "password",
+      password_confirmation: "password",
+      access_level: 100)
+
+Country.create(
+          name: 'Malaysia',
+          calling_code: '+60',
+          sms_rate: '0.1',
+          is_active: true)
