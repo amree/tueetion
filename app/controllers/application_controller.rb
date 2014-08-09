8e69097949eba5fdd7485c0037ae8b37386871f9
amree <mohd.amree@gmail.com>
2014-08-09 23:45:47 +0800

    Submit for information for Bugsnag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
   before_filter :configure_permitted_parameters, if: :devise_controller?
   before_filter :set_google_analytics_id
+  before_filter :add_user_info_to_bugsnag
   around_filter :set_time_zone
 
   protected
@@ -70,6 +71,16 @@ class ApplicationController < ActionController::Base
     if current_user && current_center.nil?
       redirect_to new_center_path
     end
+  end
+
+  def add_user_info_to_bugsnag(notif)
+    notif.user = {
+      center_name: current_center.name,
+      center_id: current_center.id,
+      email: current_user.email,
+      name: current_user.full_name,
+      id: current_user.id
+    }
   end
 
   helper_method :current_center
