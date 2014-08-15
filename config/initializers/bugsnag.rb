253c20180f8a9123b0d3419cc1711ffd79eba378
amree <mohd.amree@gmail.com>
2014-08-16 00:22:20 +0800

    Updated for Travis CI

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,7 +1,5 @@
-if Rails.env.test?
-  Bugsnag.configure do |config|
-    config.api_key = Rails.application.secrets.bugsnag_api
-    config.use_ssl = true
-    config.notify_release_stages = ["production"]
-  end
+Bugsnag.configure do |config|
+  config.api_key = Rails.application.secrets.bugsnag_api
+  config.use_ssl = true
+  config.notify_release_stages = ["production"]
 end
