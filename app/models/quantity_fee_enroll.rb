60ab677f65a7edb135ab176b6f75092b050db2a2
amree <mohd.amree@gmail.com>
2014-09-25 19:05:20 +0800

    Validates Quantity Fee Enroll's data properly. Closes #114

--- app/models/quantity_fee_enroll.rb
+++ app/models/quantity_fee_enroll.rb
@@ -2,10 +2,23 @@ class QuantityFeeEnroll < Enroll
   validates :enrollable_id, uniqueness: { scope: [:student_id, :enrollable_type] }
 
   before_validation :set_enrollable_type
+  validates :enrollable, presence: true
+  validate  :enrollable_should_be_from_same_center
   after_save :generate_enrolled_subjects
 
   def name
     enrollable.subject.name
+  end
+
+  def enrollable_should_be_from_same_center
+    unless self.enrollable.nil?
+      student_center_id = self.student.center.id
+      quantity_fee_center_id = self.enrollable.center.id
+
+      if student_center_id != quantity_fee_center_id
+        errors.add(:enrollable, "must be from the same center")
+      end
+    end
   end
 
   protected
---
+++ test/models/quantity_fee_enroll_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class QuantityFeeEnrollTest < ActiveSupport::TestCase
+  setup do
+    @center = centers(:pakar)
+  end
+
+  test "should be invalidate with no enrollable" do
+    enroll = QuantityFeeEnroll.new
+    enroll.student = @center.students.first
+
+    assert enroll.invalid?
+  end
+
+  test "should be invalidate with invalid enrollable" do
+    enroll = QuantityFeeEnroll.new
+    enroll.student = @center.students.first
+    enroll.enrollable_id   = 100
+    enroll.enrollable_type = 'QuantityFee'
+
+    assert enroll.invalid?
+  end
+
+  test "should be invalidate with other's quantity fee" do
+    quantity_fee = quantity_fees(:sejarah_quantity_fee)
+
+    enroll = QuantityFeeEnroll.new
+    enroll.student = @center.students.first
+    enroll.enrollable_id = quantity_fee.id
+    enroll.enrollable_type = 'QuantityFee'
+
+    assert enroll.invalid?
+  end
+end
