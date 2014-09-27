930638cba76942781fce97146f4bce3ecc824377
amree <mohd.amree@gmail.com>
2014-09-27 19:27:48 +0800

    Hide navbar when viewing public bill

--- app/controllers/public/students_controller.rb
+++ app/controllers/public/students_controller.rb
@@ -2,6 +2,7 @@ class Public::StudentsController < ApplicationController
   skip_before_filter :authenticate_user!
 
   def show
+    @hide_navbar = true
     @student = Student.find_by_public_key(params[:id])
   end
 end
