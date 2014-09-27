ead379127e248a09f6d4cdd8c42fd1b943f8aedb
amree <mohd.amree@gmail.com>
2014-09-28 05:01:03 +0800

    Fix typo

--- test/models/quantity_fee_enroll_test.rb
+++ test/models/quantity_fee_enroll_test.rb
@@ -5,14 +5,14 @@ class QuantityFeeEnrollTest < ActiveSupport::TestCase
     @center = centers(:pakar)
   end
 
-  test "should be invalidate with no enrollable" do
+  test "should be invalid with no enrollable" do
     enroll = QuantityFeeEnroll.new
     enroll.student = @center.students.first
 
     assert enroll.invalid?
   end
 
-  test "should be invalidate with invalid enrollable" do
+  test "should be invalid with invalid enrollable" do
     enroll = QuantityFeeEnroll.new
     enroll.student = @center.students.first
     enroll.enrollable_id   = 100
@@ -21,7 +21,7 @@ class QuantityFeeEnrollTest < ActiveSupport::TestCase
     assert enroll.invalid?
   end
 
-  test "should be invalidate with other's quantity fee" do
+  test "should be invalid with other's quantity fee" do
     quantity_fee = quantity_fees(:sejarah_quantity_fee)
 
     enroll = QuantityFeeEnroll.new
