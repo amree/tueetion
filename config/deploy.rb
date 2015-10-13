ee188bb69bab3ae17e0076d53b55ff2c544e0ff8
amree <mohd.amree@gmail.com>
2015-10-14 01:29:46 +0800

    Set Ruby verson

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :repo_url, 'git@github.com:amree/tueetion.git'
 
 # setup rvm.
 set :rbenv_type, :user
-set :rbenv_ruby, '2.1.2'
+set :rbenv_ruby, '2.2.2'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
